d5dc83f3739a9c661833770b3f38564fe62818bd
Kevin Gimbel <hallo@kevingimbel.com>
2018-02-20 09:57:06 +0100

    Add Webserver exmaple

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/synoa/helloworld"
 )
 
 func main() {
 	gr := helloworld.NewGreeter("Hello")
-	fmt.Println(gr.Greet("World"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Path[len("/"):]
+		w.Write([]byte(gr.Greet(name)))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
